855a15205136b82e56bc0e9c1880f1b1656319da
Hemal Abeyrathne <habeyra@emea.corpdir.net>
2022-03-15 18:06:09 +0100

    initial commit

--- Calculator/Controllers/WeatherForecastController.cs
+++ Calculator/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Calculator.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
